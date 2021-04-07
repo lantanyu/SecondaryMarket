f9da2f96c61f2fb5d85507c4a35e6bbc8ce27b14
lantanyu <3210462844@qq.com>
2021-04-07 16:34:53 +0800

    updata_aop

---
+++ userservice7002/src/main/java/com/example/userservice/aop/WebAspect.java
@@ -0,0 +1,27 @@
+package com.example.userservice.aop;
+
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class WebAspect {
+    @Pointcut("execution(* com.example.userservice.controller.*.*(..))")
+    public void poinCut() {
+
+    }
+    @Around("poinCut()")
+    public Object myaround(ProceedingJoinPoint pjp) throws Throwable {
+        log.info("---------------------执行Controller开始: "+ pjp.getSignature() + " 参数：" + Lists.newArrayList(pjp.getArgs()).toString());
+        Object proceed = pjp.proceed();
+        log.info("---------------------执行Controller结束: "+ pjp.getSignature() + " 返回值：" + proceed.toString());
+        return proceed;
+        //return new CommonResult<String>(200,"成功","sssdsd");
+    }
+}
